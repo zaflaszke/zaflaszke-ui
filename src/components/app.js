b372a20b34c27d682b10a2431255da63d83861c5
ernfk <ernest.filip.kret@gmail.com>
2018-05-23 21:20:04 +0200

    Add: HotModuleReplacementPlugin to webpack.

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ export class App extends Component {
     render() {
         return (
            <div>
-		   Hello UI!!
+		   Welcome to zaflaszke.pl
 		   </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
 };
